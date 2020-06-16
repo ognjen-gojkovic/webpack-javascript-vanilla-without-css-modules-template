be66a8acf67ddfcfbbc3722734e64dbd42e90eb4
Ognjen Gojković <ognjen1804@gmail.com>
2020-06-16 23:19:52 +0200

    added multiple .html files functionality

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,56 @@
 const path = require("path");
+const fs = require("fs");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const srcPath = path.resolve(__dirname, "src");
+
+function HtmlWebpackConfig(url) {
+  const templateHtml = fs.readdirSync(url);
+  const newArray = templateHtml.filter((file) => {
+    return file.includes(".html");
+  });
+  return newArray.map((file) => {
+    console.log("file: ", file);
+    if (file.match("index.html")) {
+      return new HtmlWebpackPlugin({
+        filename: `${file}`,
+        template: path.resolve(__dirname, "src", `${file}`),
+        inject: true,
+        minify: {
+          removeComments: false,
+          collapseWhitespace: false,
+          removeRedundantAttributes: false,
+          removeEmptyAttributes: false,
+          removeStyleLinkTypeAttributes: false,
+          keepClosingSlash: false,
+          minifyCSS: false,
+          minifyURLs: false,
+        },
+      });
+    } else {
+      return new HtmlWebpackPlugin({
+        filename: `${file}`,
+        template: path.resolve(__dirname, "src", `${file}`),
+        inject: true,
+        minify: {
+          removeComments: false,
+          collapseWhitespace: false,
+          removeRedundantAttributes: false,
+          removeEmptyAttributes: false,
+          removeStyleLinkTypeAttributes: false,
+          keepClosingSlash: false,
+          minifyCSS: false,
+          minifyURLs: false,
+        },
+      });
+    }
+  });
+}
+
+const outerFiles = HtmlWebpackConfig(srcPath);
 
 module.exports = merge(common, {
   output: {
@@ -18,16 +66,8 @@ module.exports = merge(common, {
     open: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-      inject: true,
-      minify: {
-        removeComments: false,
-        collapseWhitespace: false,
-      },
-    }),
     new MiniCssExtractPlugin({
       filename: "./assets/css/[name].css",
     }),
-  ],
+  ].concat(outerFiles),
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,56 @@
 const path = require("path");
+const fs = require("fs");
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssWebpackPlugin = require("mini-css-extract-plugin");
+
+const srcPath = path.resolve(__dirname, "src");
+
+function HtmlWebpackConfig(url) {
+  const templateHtml = fs.readdirSync(url);
+  const newArray = templateHtml.filter((file) => {
+    return file.includes(".html");
+  });
+  return newArray.map((file) => {
+    console.log("file: ", file);
+    if (file.match("index.html")) {
+      return new HtmlWebpackPlugin({
+        filename: `${file}`,
+        template: path.resolve(__dirname, "src", `${file}`),
+        inject: true,
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      });
+    } else {
+      return new HtmlWebpackPlugin({
+        filename: `${file}`,
+        template: path.resolve(__dirname, "src", `${file}`),
+        inject: true,
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      });
+    }
+  });
+}
+
+const outerFiles = HtmlWebpackConfig(srcPath);
 
 module.exports = merge(common, {
   output: {
@@ -11,16 +59,8 @@ module.exports = merge(common, {
     publicPath: "",
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-      },
-    }),
     new MiniCssWebpackPlugin({
       filename: "./assets/css/[name].[contentHash].css",
     }),
-  ],
+  ].concat(outerFiles),
 });
